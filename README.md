51e0fd1c6d56bac079a7ef3d9c02760013c17ef2
lincolnadym <116439267+lincolnadym@users.noreply.github.com>
2022-12-07 20:02:54 -0500

    Initial sample workflow

---
+++ README.md
@@ -0,0 +1,41 @@
+# Workflow-examples
+
+The workflow-examples contains various workflow jobs, runnable from the command line, that utilize the workflow-chain library.
+
+## Project
+
+The workflow-examples project is a source code library that discusses and illustrates how to use the workflow-chain library.
+
+# Examples
+
+The following is a list of workflow examples. As time permits, more examples will be added that illustrate key concepts of workflow-chain, new features added, etc... The examples are designed to be run within an IDE, preferably a Node IDE.
+
+## Setup
+
+In order to run the samples there is a small amount of setup to your environment.In this first step(s) with workflow-chain, we setup a simple workflow that does the following :
+
+Start by running the following command from a terminal within the IDE. This will setup ts-node as an executable on the terminal. We use ts-node, as it appears to operate the best no matter what OS (Windows, MacOS, Linux, other) is being used.
+
+```
+npm install -g ts-node typescript '@types/node'
+```
+
+You can then run a sample workflow using 1 of the included run-\*.ts files in the src/ folder.
+
+## Simple Workflow
+
+In this first step(s) with workflow-chain, we setup a simple workflow that does the following :
+
+- Reads in a large JSON
+- Parses and processes various elements from that JSON
+- Writes out a simple report on the findings.
+
+Concepts Illustrated :
+
+- Setting up a Workflow Definition
+- Chaining Tasks
+- Command line Node runtime
+- Local Node (package.json) library depedencies
+
+  "workflow-chain": "file:./workflow-chain-1.0.1.tgz"
+  "workflow-chain": "^1.0.1"
---
+++ test/models/workflow.test.ts
@@ -0,0 +1,251 @@
+import { WFJSONParser, NSWFJSONParser } from '../../src/workflows';
+import { mockWFDefinition } from '../__mock__/common';
+import { IWorkflowContext, TWorkflowTask, Logger as Log } from 'workflow-chain';
+
+const testDefinitions = mockWFDefinition;
+
+describe('Task Objects', () => {
+  let theWorkflow: WFJSONParser;
+  beforeEach(() => {
+    theWorkflow = new WFJSONParser(NSWFJSONParser.sampleWorkflow);
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  test('should allocate the workflow with definition correctly', async () => {
+    expect(theWorkflow.getWorkflowAttributes()).toMatchObject({
+      workflowName: 'WorkflowSample',
+      workflowDescription: 'Implements a sample workflow',
+      workflowContext: {
+        payload: {
+          contextKey: 'data',
+          contextValue: {
+            one: '111',
+            two: '222',
+          },
+          statusCode: '0',
+        },
+      },
+      workflowNamespace: 'SampleWorkflow',
+      workflowStart: 'WorkflowTask001',
+    });
+  });
+  test('should validate the workflow correctly', async () => {
+    const wfValidation = theWorkflow.doValidation();
+
+    expect(wfValidation.value).toBeDefined();
+    expect(wfValidation.error).toBeUndefined();
+  });
+  test('should return starting class when it exists', async () => {
+    expect(theWorkflow.getWorkflowStartTask()).toBe('WorkflowTask001');
+  });
+  test('should allocate a workflow with task instances', async () => {
+    theWorkflow.doRunWorkflow();
+
+    expect(theWorkflow).toBeDefined();
+    Object.keys(theWorkflow.getTaskDictionary()).forEach((key, task) => {
+      expect(
+        theWorkflow.getTaskDictionary()[key] instanceof TWorkflowTask,
+      ).toBe(true);
+      Log.info(
+        `Task Key [${key}] Dictionary [${theWorkflow.getTaskDictionary()[key]}`,
+      ); // All fine!
+      // if (theWorkflow.isKey(theWorkflow.getTaskDictionary()[key], key)) {
+      // }
+    });
+  });
+  test('should allocate a workflow with a task instance with a task definition', async () => {
+    Log.info('- theWorkflow.getTaskDictionary -');
+    Log.info(theWorkflow.getTaskDictionary()['WorkflowTask001']);
+    expect(theWorkflow.getTaskDictionary()).toBeDefined();
+    expect(
+      theWorkflow.getTaskDictionary()['WorkflowTask001'] instanceof
+        TWorkflowTask,
+    ).toBe(true);
+    expect(theWorkflow.getTaskDictionary()['WorkflowTask001']).toMatchObject({
+      taskDefinition: {
+        taskSequence: 1,
+        taskId: 'workflow-001',
+        taskName: 'WorkflowTask001',
+        taskClass: 'WorkflowTask001',
+        nextTasks: ['WorkflowTask002'],
+        taskActive: true,
+      },
+    });
+  });
+  test('should build a workflow context as a string indexed dictionary', async () => {
+    let wfContext: IWorkflowContext = {
+      task001: {
+        contextKey: 'wfTask001',
+        contextValue: { eleOne: 'elementOne' },
+        statusCode: 'SUCCESS',
+      },
+    };
+
+    expect(wfContext).toMatchObject({
+      task001: {
+        contextKey: 'wfTask001',
+        contextValue: { eleOne: 'elementOne' },
+        statusCode: 'SUCCESS',
+      },
+    });
+
+    wfContext = {
+      ...wfContext,
+      task002: {
+        contextKey: 'wfTask002',
+        contextValue: { eleOne: 'elementOne' },
+        statusCode: 'SUCCESS',
+      },
+    };
+
+    expect(wfContext).toMatchObject({
+      task001: {
+        contextKey: 'wfTask001',
+        contextValue: { eleOne: 'elementOne' },
+        statusCode: 'SUCCESS',
+      },
+      task002: {
+        contextKey: 'wfTask002',
+        contextValue: { eleOne: 'elementOne' },
+        statusCode: 'SUCCESS',
+      },
+    });
+  });
+  test('setWorkflowContext should set the workflow context', async () => {
+    let wfContext: IWorkflowContext = {
+      task001: {
+        contextKey: 'wfTask001',
+        contextValue: { eleOne: 'elementOne' },
+        statusCode: 'SUCCESS',
+      },
+    };
+
+    expect(theWorkflow.getWorkflowContext()).toBeDefined();
+    theWorkflow.setWorkflowContext(wfContext);
+    expect(theWorkflow.getWorkflowContext()).toMatchObject({
+      task001: {
+        contextKey: 'wfTask001',
+        contextValue: { eleOne: 'elementOne' },
+        statusCode: 'SUCCESS',
+      },
+    });
+  });
+  test('doPreWorkflow should add default context to the workflow context', async () => {
+    expect(theWorkflow.getWorkflowContext()).toMatchObject({});
+    theWorkflow.doPreWorkflow();
+    expect(theWorkflow.getWorkflowContext()).toMatchObject({
+      'WorkflowSample:doPreWorkflow': {
+        contextKey: 'WorkflowSample:doPreWorkflow',
+        contextValue: {},
+        statusCode: 'SUCCESS',
+      },
+    });
+  });
+  test('doPostWorkflow should add default context to the workflow context', async () => {
+    expect(theWorkflow.getWorkflowContext()).toMatchObject({});
+    theWorkflow.doPostWorkflow();
+    expect(theWorkflow.getWorkflowContext()).toMatchObject({
+      'WorkflowSample:doPostWorkflow': {
+        contextKey: 'WorkflowSample:doPostWorkflow',
+        contextValue: {},
+        statusCode: 'SUCCESS',
+      },
+    });
+  });
+  test('setWorkflowContext should set/override the current workflow context', async () => {
+    let wfContext: IWorkflowContext = {
+      wfTask001: {
+        contextKey: 'wfTask001',
+        contextValue: { eleOne: 'elementOne' },
+        statusCode: 'SUCCESS',
+      },
+    };
+
+    expect(theWorkflow.getWorkflowContext()).toMatchObject({});
+    theWorkflow.doPreWorkflow();
+    expect(theWorkflow.getWorkflowContext()).toMatchObject({
+      'WorkflowSample:doPreWorkflow': {
+        contextKey: 'WorkflowSample:doPreWorkflow',
+        contextValue: {},
+        statusCode: 'SUCCESS',
+      },
+    });
+    theWorkflow.setWorkflowContext(wfContext);
+    expect(theWorkflow.getWorkflowContext()).toMatchObject({
+      wfTask001: {
+        contextKey: 'wfTask001',
+        contextValue: { eleOne: 'elementOne' },
+        statusCode: 'SUCCESS',
+      },
+    });
+  });
+  test('getContextItem should return correct indexed context item', async () => {
+    expect(theWorkflow.getWorkflowContext()).toMatchObject({});
+    theWorkflow.doPreWorkflow();
+    expect(theWorkflow.getWorkflowContext()).toMatchObject({
+      'WorkflowSample:doPreWorkflow': {
+        contextKey: 'WorkflowSample:doPreWorkflow',
+        contextValue: {},
+        statusCode: 'SUCCESS',
+      },
+    });
+    expect(
+      theWorkflow.getContextItem('WorkflowSample:doPreWorkflow'),
+    ).toMatchObject({
+      contextKey: 'WorkflowSample:doPreWorkflow',
+      contextValue: {},
+      statusCode: 'SUCCESS',
+    });
+  });
+  test('should build the workflow context as tasks execute', async () => {
+    expect(theWorkflow.getWorkflowContext()).toMatchObject({});
+    theWorkflow.doPreWorkflow();
+    expect(theWorkflow.getWorkflowContext()).toMatchObject({
+      'WorkflowSample:doPreWorkflow': {
+        contextKey: 'WorkflowSample:doPreWorkflow',
+        contextValue: {},
+        statusCode: 'SUCCESS',
+      },
+    });
+    Log.info('- theWorkflow.doRunWorkflow - Context - Before -');
+    Log.info(theWorkflow.getWorkflowContext());
+    theWorkflow.doRunWorkflow();
+    Log.info('- theWorkflow.doRunWorkflow - Context - After -');
+    Log.info(theWorkflow.getWorkflowContext());
+    expect(theWorkflow.getWorkflowContext()).toMatchObject({
+      'WorkflowSample:doPreWorkflow': {
+        contextKey: 'WorkflowSample:doPreWorkflow',
+        contextValue: {},
+        statusCode: 'SUCCESS',
+      },
+      'WorkflowTask001:doPreTask': {
+        contextKey: 'WorkflowTask001:doPreTask',
+        contextValue: {},
+        statusCode: 'SUCCESS',
+      },
+      'WorkflowTask001:doRunTask': {
+        contextKey: 'WorkflowTask001:doRunTask',
+        contextValue: {},
+        statusCode: 'SUCCESS',
+      },
+      'WorkflowTask001:doPostTask': {
+        contextKey: 'WorkflowTask001:doPostTask',
+        contextValue: {},
+        statusCode: 'SUCCESS',
+      },
+      'WorkflowTask002:doPreTask': {
+        contextKey: 'WorkflowTask002:doPreTask',
+        contextValue: {},
+        statusCode: 'SUCCESS',
+      },
+      'WorkflowTask002:doPostTask': {
+        contextKey: 'WorkflowTask002:doPostTask',
+        contextValue: {},
+        statusCode: 'SUCCESS',
+      },
+    });
+  });
+});
